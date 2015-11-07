d4d121798c868c6ff436c2b85b95752708b7fca3
Brad Campbell <bradjc5@gmail.com>
2015-11-07 15:41:10 +0900

    updated mbramfs to be closer to actual API
    more checks, add append, fix bugs

--- lib/mbramfs.c
+++ lib/mbramfs.c
@@ -3,27 +3,76 @@
 // Supports 1 file
 
 
-#include "stdio.h"
-#include "stdint.h"
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <string.h>
 
 #define MBRAMFS_FILESIZE_IN_BYTES 10000
 
 FILE onefile;
 
+static uint8_t  fexists = 0;
+static uint8_t  fisopen = 0;
 static uint32_t fptr = 0;
-static uint8_t filebuf[MBRAMFS_FILESIZE_IN_BYTES] = {0};
+static uint8_t  filebuf[MBRAMFS_FILESIZE_IN_BYTES] = {0};
+static uint32_t flen = 0;
+
+static uint8_t  read_enabled = 0;
+static uint8_t  write_enabled = 0;
 
 
 FILE* fopen (const char* restrict fname, const char* restrict flags) {
+	uint8_t read = 0;
+	uint8_t write = 0;
+	uint8_t append = 0;
+
+	int i = 0;
+
+	// Parse the flags
+	while (flags[i]) {
+		if      (flags[i] == 'w') write = 1;
+		else if (flags[i] == 'r') read = 1;
+		else if (flags[i] == 'a') append = 1;
+
+		i++;
+	}
+
+	// Write has priority (?)
+	if (write && append) {
+		append = 0;
+	}
+
+	// Cannot read from a file that does not exist
+	if (read && !fexists) return NULL;
+
+	// Writing a file makes it exist
+	if (read) {
+		fptr = 0;
+	} else if (write) {
+		fexists = 1;
+		flen = 0;
+		fptr = 0;
+	} else if (append) {
+		fexists = 1;
+		fptr = flen;
+	}
+
+	read_enabled = read;
+	write_enabled = write | append;
+
+	fisopen = 1;
 	return &onefile;
 }
 
 size_t fread (void* ptr, size_t size, size_t count, FILE* stream) {
 	uint32_t copy_len = size*count;
 
-	// Make sure we don't read off the end of the array
-	if (fptr + copy_len > MBRAMFS_FILESIZE_IN_BYTES) {
-		copy_len = MBRAMFS_FILESIZE_IN_BYTES - fptr;
+	if (!fisopen || !read_enabled) return 0;
+
+	// Make sure we don't read past the end of the file
+	if (fptr + copy_len > flen) {
+		copy_len = flen - fptr;
 	}
 
 	// memcpy the "file" to the user buffer and return how much we copied
@@ -35,6 +84,8 @@ size_t fread (void* ptr, size_t size, size_t count, FILE* stream) {
 size_t fwrite (const void* ptr, size_t size, size_t count, FILE* stream) {
 	uint32_t write_len = size*count;
 
+	if (!fisopen || !write_enabled) return 0;
+
 	// Make sure we don't read off the end of the array
 	if (fptr + write_len > MBRAMFS_FILESIZE_IN_BYTES) {
 		write_len = MBRAMFS_FILESIZE_IN_BYTES - fptr;
@@ -42,6 +93,7 @@ size_t fwrite (const void* ptr, size_t size, size_t count, FILE* stream) {
 
 	memcpy(filebuf+fptr, ptr, write_len);
 	fptr += write_len;
+	flen += write_len;
 	return write_len;
 }
 
@@ -55,7 +107,7 @@ int fseek (FILE* f, long int offset, int origin) {
 		new_position = offset + fptr;
 	}
 
-	if (new_position > MBRAMFS_FILESIZE_IN_BYTES) {
+	if (new_position > flen) {
 		// Seek too far, past the end of the "file"
 		return -1;
 	}
@@ -71,6 +123,7 @@ void rewind (FILE* f) {
 }
 
 int fclose (FILE* stream) {
+	fisopen = 0;
 	return 0;
 }
 
@@ -78,4 +131,7 @@ int fclose (FILE* stream) {
 int remove (const char* filename) {
 	memset(filebuf, 0, MBRAMFS_FILESIZE_IN_BYTES);
 	fptr = 0;
+	flen = 0;
+	fexists = 0;
+	return 0;
 }
