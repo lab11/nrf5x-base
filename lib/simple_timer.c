2dd6a60bff0798efbf2c24a63724c868284ca665
Brad Campbell <bradjc5@gmail.com>
2015-12-01 20:44:37 -0500

    cleaned up old code/comments

--- lib/simple_timer.c
+++ lib/simple_timer.c
@@ -1,4 +1,3 @@
-
 #include <stdint.h>
 #include <stdbool.h>
 #include "nordic_common.h"
@@ -6,9 +5,6 @@
 
 #define SIMPLE_TIMER_PRESCALER     0
 #define SIMPLE_TIMER_OP_QUEUE_SIZE 4
-
-
-#define SIMPLE_TIMER_INSTANTIATE(timer_name) APP_TIMER_DEF(timer_name)
 
 // Define four timers that the app can use.
 // This is a bit of a hack, but makes for a really simple API.
@@ -56,49 +52,3 @@ uint32_t simple_timer_start (uint32_t milliseconds,
 	                           NULL);
 	return err_code;
 }
-
-// uint32_t simple_timer_start (app_timer_id_t const* timer,
-//                              uint32_t milliseconds,
-//                              app_timer_timeout_handler_t callback) {
-
-// 	uint32_t err_code;
-
-// 	err_code = app_timer_create(timer, APP_TIMER_MODE_REPEATED, callback);
-// 	if (err_code != NRF_SUCCESS) return err_code;
-
-// 	err_code = app_timer_start(*timer,
-// 	                           APP_TIMER_TICKS(milliseconds, SIMPLE_TIMER_PRESCALER),
-// 	                           NULL);
-// 	return err_code;
-// }
-
-
-
-/* EXAMPLE
-
-SIMPLE_TIMER_INSTANTIATE(my_timer);
-
-static void timer_handler (void* p_context) {
-	...
-}
-
-int main () {
-	uint32_t err;
-
-	simple_timer_init();
-	err = simple_timer_start(&my_timer, 500, timer_handler);
-
-	while (1);
-}
-
-*/
-
-
-/* TODO
-
-Just allocate some timers here and get the API I want
-
-init();
-start(100, callback);
-
-*/
