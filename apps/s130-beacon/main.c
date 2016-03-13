5fbc955bc60d3bb7fd0a59fe862503a09c5970cd
Brad Campbell <bradjc5@gmail.com>
2016-03-12 19:26:13 -0500

    add s130 test app

---
+++ apps/s130-beacon/main.c
@@ -0,0 +1,40 @@
+/*
+ * Send an advertisement periodically
+ */
+
+#include <stdbool.h>
+#include <stdint.h>
+#include "nrf_gpio.h"
+#include "ble_advdata.h"
+#include "nordic_common.h"
+#include "softdevice_handler.h"
+#include "ble_debug_assert_handler.h"
+#include "led.h"
+
+#include "simple_ble.h"
+#include "simple_adv.h"
+
+// Intervals for advertising and connections
+static simple_ble_config_t ble_config = {
+    .platform_id       = 0x00,              // used as 4th octet in device BLE address
+    .device_id         = DEVICE_ID_DEFAULT,
+    .adv_name          = "SDK11",
+    .adv_interval      = MSEC_TO_UNITS(500, UNIT_0_625_MS),
+    .min_conn_interval = MSEC_TO_UNITS(500, UNIT_1_25_MS),
+    .max_conn_interval = MSEC_TO_UNITS(1000, UNIT_1_25_MS)
+};
+
+
+int main(void) {
+    uint32_t err_code;
+
+    // Setup BLE
+    simple_ble_init(&ble_config);
+
+    // Advertise because why not
+    simple_adv_only_name();
+
+    while (1) {
+        power_manage();
+    }
+}
