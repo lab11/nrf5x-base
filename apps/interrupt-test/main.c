fe3fa6762ed40723da8a348b5078460345c8e67f
Will Huang <wwhuang@umich.edu>
2016-05-05 15:39:53 -0400

    updated interrupt example app to use new GPIOTE driver

--- apps/interrupt-test/main.c
+++ apps/interrupt-test/main.c
@@ -6,42 +6,45 @@
 #include "led.h"
 #include "nordic_common.h"
 #include "softdevice_handler.h"
-#include "app_gpiote.h"
+#include "nrf_drv_gpiote.h"
+#include "simple_ble.h"
 
-// Need pin number for LED
-#define LED 18
+
+// Need pin number for red LED on nucleum
+#define LED_RED 18
+#define LED_BLUE 19
 
 // Interrupt pin number
-#define INTERRUPT_PIN 10
+#define INTERRUPT_PIN 23
 
-// Need this for the app_gpiote library
-app_gpiote_user_id_t gpiote_user;
-
-
-void interrupt_handler (uint32_t pins_l2h, uint32_t pins_h2l) {
-    led_toggle(LED);
+static void interrupt_handler (nrf_drv_gpiote_pin_t pin, nrf_gpiote_polarity_t action) {
+    if(pin == INTERRUPT_PIN) {led_toggle(LED_BLUE);} // Pin is returned to use as decimal number and not a bitmap. fuck yeah!
+    led_toggle(LED_RED);
 }
 
 int main(void) {
 
     // Initialize.
-    led_init(LED);
-    led_off(LED);
+    led_init(LED_RED);
+    led_init(LED_BLUE);
 
-    // For 3 users of GPIOTE
-    APP_GPIOTE_INIT(3);
+    // Initialize GPIOTE driver
+    // configuration values can be found in nrf_drv_config.h under GPIOTE_ENABLED
+    uint32_t err_code;
+    if(!nrf_drv_gpiote_is_init()) {
+        err_code = nrf_drv_gpiote_init();
+        if(err_code != NRF_SUCCESS) { /* Handle failure case*/ }
+    }
 
-    // Register us as one user
-    app_gpiote_user_register(&gpiote_user,
-                             1<<INTERRUPT_PIN,   // Which pins we want the interrupt for low to high
-                             1<<INTERRUPT_PIN,   // Which pins we want the interrupt for high to low
-                             interrupt_handler);
-
-    // Ready to go!
-    app_gpiote_user_enable(gpiote_user);
+    // Initialize interrupt on pin 23.
+    nrf_drv_gpiote_in_config_t test_config = GPIOTE_CONFIG_IN_SENSE_HITOLO(false);
+    test_config.pull = NRF_GPIO_PIN_PULLUP;
+    err_code = nrf_drv_gpiote_in_init(23, &test_config, interrupt_handler);
+    if(err_code != NRF_SUCCESS) { /* Handle failure case */ }
+    nrf_drv_gpiote_in_event_enable(23, true); // enable interrupt
 
     // Enter main loop.
     while (1) {
-        sd_app_evt_wait();
+        power_manage();
     }
 }
