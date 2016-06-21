5accaf1dc7dc77ee96d606354b189e20474a9867
Cole Hudson <cmhudson11@gmail.com>
2016-06-20 22:01:18 -0400

    The pixel change function is much more precise

--- apps/eink-advanced/convert_to_epd.py
+++ apps/eink-advanced/convert_to_epd.py
@@ -1,7 +1,7 @@
 from PIL import Image
-image_file = Image.open('lab11_eink_color.png') # open colour image
+image_file = Image.open('friends.jpg') # open colour image
 image_file = image_file.convert('1') # convert image to black and white
-image_file.save('result.png')
+image_file.save('result.jpg')
 
 pixels = image_file.load()
 
