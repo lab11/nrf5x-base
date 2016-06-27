81e00f28e41115ddc5afb0db523b78c5e9e700dd
Josh Adkins <adkinsjd@umich.edu>
2016-06-27 18:03:03 -0400

    simplifying for debugging

--- lib/simple_logger/simple_logger.c
+++ lib/simple_logger/simple_logger.c
@@ -41,24 +41,23 @@ static void heartbeat (void* p_context) {
 //let's reopen the file, and try to rewrite the header if it's necessary
 static uint8_t logger_init() {
 
-	f_mount(&simple_logger_fs, "", 1);
+	FRESULT res;
+	res = f_mount(&simple_logger_fs, "", 1);
 
-	
-	
-	//see if the file exists already
+/*	//see if the file exists already
 	FIL temp;
-	FRESULT res = f_open(&temp,file, FA_READ | FA_OPEN_EXISTING);
+	res = f_open(&temp,file, FA_READ | FA_OPEN_EXISTING);
 	if(res == FR_NO_FILE) {
 		//the file doesn't exist
 		simple_logger_file_exists = 0;
 	} else {
 		simple_logger_file_exists = 1;
 		f_close(&temp);
-	}
+	}*/
 
 	res = f_open(&simple_logger_fpointer,file, simple_logger_opts);
 
-	if(simple_logger_opts && FA_OPEN_ALWAYS) {
+	/*if(simple_logger_opts && FA_OPEN_ALWAYS) {
 		//we are in append mode and should move to the end
 		res = f_lseek(&simple_logger_fpointer, f_size(&simple_logger_fpointer));
 	}
@@ -66,7 +65,7 @@ static uint8_t logger_init() {
 	if(header_written && !simple_logger_file_exists) {
 		uint8_t err = f_puts(header_buffer, &simple_logger_fpointer);
 		res = f_sync(&simple_logger_fpointer);
-	}
+	}*/
 
 	simple_logger_inited = 1;
 	return SIMPLE_LOGGER_SUCCESS;
