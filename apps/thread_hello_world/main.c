3031e323ce8f5c5e04d7212e87a89c21c2c9649c
Neal Jackson <nealjack@umich.edu>
2018-06-22 18:25:10 -0700

    Thread hello world app working
    Simply starts up, joins network if available

--- apps/thread_hello_world/main.c
+++ apps/thread_hello_world/main.c
@@ -9,9 +9,10 @@
 #include "nrf_timer.h"
 #include "nrf_delay.h"
 #include "nrf_gpio.h"
-#include "app_uart.h"
-#include "nrf_uarte.h"
 #include "nrf_power.h"
+#include "nrf_log.h"
+#include "nrf_log_ctrl.h"
+#include "nrf_log_default_backends.h"
 
 #include <openthread/openthread.h>
 #include <openthread/icmp6.h>
@@ -19,11 +20,6 @@
 #include <openthread/cli.h>
 #include <openthread/diag.h>
 
-
-#define UART_RX              26
-#define UART_TX              27
-#define UART_TX_BUF_SIZE     256
-#define UART_RX_BUF_SIZE     256
 
 #define LED0 NRF_GPIO_PIN_MAP(0,4)
 #define LED1 NRF_GPIO_PIN_MAP(0,5)
@@ -40,39 +36,18 @@
 static otInstance * mp_ot_instance;
 static otNetifAddress m_slaac_addresses[NUM_SLAAC_ADDRESSES];               /**< Buffer containing addresses resolved by SLAAC */
 
+/**@brief Function for initializing the nrf log module.
+ */
+static void log_init(void)
+{
+    ret_code_t err_code = NRF_LOG_INIT(NULL);
+    APP_ERROR_CHECK(err_code);
+
+    NRF_LOG_DEFAULT_BACKENDS_INIT();
+}
+
 /**@brief Function for initializing the Thread Stack.
  */
-
-void uart_error_handle (app_uart_evt_t * p_event) {
-    if (p_event->evt_type == APP_UART_COMMUNICATION_ERROR) {
-        APP_ERROR_HANDLER(p_event->data.error_communication);
-    } else if (p_event->evt_type == APP_UART_FIFO_ERROR) {
-        APP_ERROR_HANDLER(p_event->data.error_code);
-    }
-}
-
-void uart_init(void) {
-  uint32_t err_code;
-
-  const app_uart_comm_params_t comm_params =
-  {
-    UART_RX,
-    UART_TX,
-    0,
-    0,
-    APP_UART_FLOW_CONTROL_DISABLED,
-    false,
-    NRF_UARTE_BAUDRATE_115200
-  };
-  APP_UART_FIFO_INIT(&comm_params,
-                     UART_RX_BUF_SIZE,
-                     UART_TX_BUF_SIZE,
-                     uart_error_handle,
-                     APP_IRQ_PRIORITY_LOW,
-                     err_code);
-  APP_ERROR_CHECK(err_code);
-
-}
 
 void thread_init()
 {
@@ -83,19 +58,19 @@ void thread_init()
     mp_ot_instance = otInstanceInitSingle();
     ASSERT(mp_ot_instance != NULL);
 
-    printf("Thread version: %s\r\n", otGetVersionString());
-    printf("Network name:   %s\r\n",
+    NRF_LOG_INFO("Thread version: %s", otGetVersionString());
+    NRF_LOG_INFO("Network name:   %s",
                  otThreadGetNetworkName(mp_ot_instance));
 
-    if (!otDatasetIsCommissioned(mp_ot_instance) && AUTOCOMMISSION)
+    if (!otDatasetIsCommissioned(mp_ot_instance) || AUTOCOMMISSION)
     {
         error = otLinkSetChannel(mp_ot_instance, THREAD_CHANNEL);
         ASSERT(error == OT_ERROR_NONE);
-        printf("Thread Channel: %d\r\n", otLinkGetChannel(mp_ot_instance));
+        NRF_LOG_INFO("Thread Channel: %d", otLinkGetChannel(mp_ot_instance));
 
         error = otLinkSetPanId(mp_ot_instance, THREAD_PANID);
         ASSERT(error == OT_ERROR_NONE);
-        printf("Thread PANID: %lx\r\n", (uint32_t)otLinkGetPanId(mp_ot_instance));
+        NRF_LOG_INFO("Thread PANID: 0x%lx", (uint32_t)otLinkGetPanId(mp_ot_instance));
     }
 
     otLinkModeConfig mode;
@@ -115,28 +90,25 @@ void thread_init()
 
     otThreadSetChildTimeout(mp_ot_instance, DEFAULT_CHILD_TIMEOUT);
 
-    //if (!p_thread_configuration->autostart_disable)
-    //{
-    //    error = otIp6SetEnabled(mp_ot_instance, true);
-    //    ASSERT(error == OT_ERROR_NONE);
+    error = otIp6SetEnabled(mp_ot_instance, true);
+    ASSERT(error == OT_ERROR_NONE);
 
-    //    if (otDatasetIsCommissioned(mp_ot_instance) || p_thread_configuration->autocommissioning)
-    //    {
-    //        error = otThreadSetEnabled(mp_ot_instance, true);
-    //        ASSERT(error == OT_ERROR_NONE);
+    if (otDatasetIsCommissioned(mp_ot_instance) || AUTOCOMMISSION)
+    {
+      error = otThreadSetEnabled(mp_ot_instance, true);
+      ASSERT(error == OT_ERROR_NONE);
 
-    //        NRF_LOG_INFO("Thread interface has been enabled.");
-    //        NRF_LOG_INFO("802.15.4 Channel: %d", otLinkGetChannel(mp_ot_instance));
-    //        NRF_LOG_INFO("802.15.4 PAN ID:  0x%04x", otLinkGetPanId(mp_ot_instance));
-    //        NRF_LOG_INFO("rx-on-when-idle:  %s", otThreadGetLinkMode(mp_ot_instance).mRxOnWhenIdle ?
-    //                                        "enabled" : "disabled");
-    //    }
-    //}
+      NRF_LOG_INFO("Thread interface has been enabled.");
+      NRF_LOG_INFO("802.15.4 Channel: %d", otLinkGetChannel(mp_ot_instance));
+      NRF_LOG_INFO("802.15.4 PAN ID:  0x%04x", otLinkGetPanId(mp_ot_instance));
+      NRF_LOG_INFO("rx-on-when-idle:  %s", otThreadGetLinkMode(mp_ot_instance).mRxOnWhenIdle ?
+          "enabled" : "disabled");
+    }
 }
 
 static void state_changed_callback(uint32_t flags, void * p_context)
 {
-    printf("State changed! Flags: 0x%08lx Current role: %d\r\n",
+    NRF_LOG_INFO("State changed! Flags: 0x%08lx Current role: %d\r\n",
                  flags, otThreadGetDeviceRole(p_context));
 
     if (flags & OT_CHANGED_THREAD_NETDATA)
@@ -196,11 +168,11 @@ void thread_sleep(void)
 int main(void) {
     nrf_power_dcdcen_set(1);
 
+    log_init();
+
     // Initialize.
     nrf_gpio_cfg_output(LED2);
     nrf_gpio_pin_set(LED2);
-
-    uart_init();
 
     thread_init();
     otError error = otSetStateChangedCallback(mp_ot_instance, state_changed_callback, mp_ot_instance);
@@ -208,8 +180,9 @@ int main(void) {
     // Enter main loop.
     while (1) {
         thread_process();
-        thread_sleep();
-        //nrf_delay_ms(500);
-        nrf_gpio_pin_toggle(LED2);
+        if (NRF_LOG_PROCESS() == false)
+        {
+          thread_sleep();
+        }
     }
 }
