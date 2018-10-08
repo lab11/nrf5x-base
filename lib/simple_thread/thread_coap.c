7f42d5d9d36af96fbbd2c5c943d28b3cf7c4ce23
Neal Jackson <nealjack@umich.edu>
2018-10-08 11:41:16 -0700

    Thread coap return error

--- lib/simple_thread/thread_coap.c
+++ lib/simple_thread/thread_coap.c
@@ -32,56 +32,52 @@ void thread_coap_client_init(otInstance* instance) {
     otCoapSetDefaultHandler(instance, thread_coap_handler, NULL);
 }
 
-void thread_coap_send(otInstance* instance, otCoapCode req, otCoapType type, const otIp6Address* dest, const char* path, const uint8_t* data, size_t len) {
-    otError       error = OT_ERROR_NONE;
-    otMessage   * message;
-    otMessageInfo message_info;
-    otCoapHeader  header;
+otError thread_coap_send(otInstance* instance, otCoapCode req, otCoapType type, const otIp6Address* dest, const char* path, const uint8_t* data, size_t len) {
+  otError       error = OT_ERROR_NONE;
+  otMessage   * message;
+  otMessageInfo message_info;
+  otCoapHeader  header;
 
-    do
-    {
-        if (otIp6IsAddressEqual(dest, &m_unspecified_ipv6))
-        {
-            NRF_LOG_INFO("Failed to send the CoAP Request to the Unspecified IPv6 Address\r\n");
-            break;
-        }
+  if (otIp6IsAddressEqual(dest, &m_unspecified_ipv6))
+  {
+    NRF_LOG_INFO("Failed to send the CoAP Request to the Unspecified IPv6 Address\r\n");
+    return OT_ERROR_INVALID_ARGS;
+  }
 
-        otCoapHeaderInit(&header, type, req);
-        otCoapHeaderGenerateToken(&header, 2);
-        otCoapHeaderAppendUriPathOptions(&header, path);
-        otCoapHeaderSetPayloadMarker(&header);
+  otCoapHeaderInit(&header, type, req);
+  otCoapHeaderGenerateToken(&header, 2);
+  otCoapHeaderAppendUriPathOptions(&header, path);
+  otCoapHeaderSetPayloadMarker(&header);
 
-        message = otCoapNewMessage(instance, &header);
-        if (message == NULL)
-        {
-            NRF_LOG_INFO("Failed to allocate message for CoAP Request\r\n");
-            // Force an app error and subsequent reset - this shouldn't happen!
-            APP_ERROR_CHECK_BOOL(message != NULL);
-            break;
-        }
+  message = otCoapNewMessage(instance, &header);
+  if (message == NULL)
+  {
+    NRF_LOG_INFO("Failed to allocate message for CoAP Request\r\n");
+    // Force an app error and subsequent reset - this shouldn't happen!
+    return OT_ERROR_NO_BUFS;
+  }
 
-        error = otMessageAppend(message, data, len);
-        if (error != OT_ERROR_NONE)
-        {
-            break;
-        }
+  error = otMessageAppend(message, data, len);
+  if (error != OT_ERROR_NONE)
+  {
+    return error;
+  }
 
-        memset(&message_info, 0, sizeof(message_info));
-        message_info.mInterfaceId = OT_NETIF_INTERFACE_ID_THREAD;
-        message_info.mPeerPort    = OT_DEFAULT_COAP_PORT;
-        memcpy(&message_info.mPeerAddr, dest, sizeof(message_info.mPeerAddr));
+  memset(&message_info, 0, sizeof(message_info));
+  message_info.mInterfaceId = OT_NETIF_INTERFACE_ID_THREAD;
+  message_info.mPeerPort    = OT_DEFAULT_COAP_PORT;
+  memcpy(&message_info.mPeerAddr, dest, sizeof(message_info.mPeerAddr));
 
-        error = otCoapSendRequest(instance,
-                                  message,
-                                  &message_info,
-                                  NULL,
-                                  instance);
-    } while (false);
+  error = otCoapSendRequest(instance,
+      message,
+      &message_info,
+      NULL,
+      instance);
 
-    if (error != OT_ERROR_NONE && message != NULL)
-    {
-        NRF_LOG_INFO("Failed to send CoAP Request: %d\r\n", error);
-        otMessageFree(message);
-    }
-
+  if (error != OT_ERROR_NONE && message != NULL)
+  {
+    NRF_LOG_INFO("Failed to send CoAP Request: %d\r\n", error);
+    otMessageFree(message);
+  }
+  return error;
 }
--- lib/simple_thread/thread_coap.h
+++ lib/simple_thread/thread_coap.h
@@ -6,4 +6,4 @@
 #include <openthread/coap.h>
 
 void thread_coap_client_init(otInstance* instance);
-void thread_coap_send(otInstance* instance, otCoapCode req, otCoapType type, const otIp6Address* dest, const char* path, const uint8_t* data, size_t len);
+otError thread_coap_send(otInstance* instance, otCoapCode req, otCoapType type, const otIp6Address* dest, const char* path, const uint8_t* data, size_t len);
